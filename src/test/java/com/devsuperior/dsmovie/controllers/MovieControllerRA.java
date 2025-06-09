627617a04c02ab49c9948480b90e781514fe4e49
Luis Parente <lgsp25@hotmail.com>
2025-06-09 19:06:30 -0300

    MovieControllerRA ok

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,14 +1,20 @@
 package com.devsuperior.dsmovie.controllers;
 
 import org.json.JSONException;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.devsuperior.dsmovie.tests.TokenUtil;
 
+import io.restassured.http.ContentType;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+
+import java.util.Map;
+import java.util.HashMap;
 
 public class MovieControllerRA {
 	
@@ -19,6 +25,10 @@ public class MovieControllerRA {
 	private String adminToken, clientToken, invalidToken;
 	
 	private String validMovieTitle;
+	
+	private Long existingMovieId, nonExistingMovieId;
+	
+	private Map<String, Object> postMovieInstance;
 	
 	@BeforeEach
 	public void setUp() throws Exception{
@@ -40,6 +50,17 @@ public class MovieControllerRA {
 		invalidToken = adminToken + "xpto";
 		
 		validMovieTitle = "Carnificina";
+		
+		existingMovieId = 1L;
+		
+		nonExistingMovieId = 100L;
+		
+		postMovieInstance = new HashMap<>();
+		
+		postMovieInstance.put("title", "Test Movie");
+		postMovieInstance.put("score", 0.0);
+		postMovieInstance.put("count", 0);
+		postMovieInstance.put("image", "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg");
 	}
 	
 	@Test
@@ -62,21 +83,72 @@ public class MovieControllerRA {
 	
 	@Test
 	public void findByIdShouldReturnMovieWhenIdExists() {		
+		given()
+			.get("/movies/{id}", existingMovieId)
+		.then()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("title", equalTo("The Witcher"))
+			.body("score", is(4.5F))
+			.body("count", is(2))
+			.body("image", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg"));
 	}
 	
 	@Test
 	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {	
+		given()
+			.get("/movies/{id}", nonExistingMovieId)
+		.then()
+			.statusCode(404);
 	}
 	
 	@Test
 	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {		
+		postMovieInstance.put("title", " ");
+		
+		JSONObject newMovie = new JSONObject(postMovieInstance);
+		
+		given()
+			.header("Content-type", "application/json")
+			.header("Authorization", "Bearer " + adminToken)
+			.body(newMovie)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post("/movies")
+		.then()
+			.statusCode(422);
 	}
 	
 	@Test
 	public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
+		JSONObject newMovie = new JSONObject(postMovieInstance);
+		
+		given()
+			.header("Content-type", "application/json")
+			.header("Authorization", "Bearer " + clientToken)
+			.body(newMovie)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post("/movies")
+		.then()
+			.statusCode(403);
 	}
 	
 	@Test
 	public void insertShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
+		JSONObject newMovie = new JSONObject(postMovieInstance);
+		
+		given()
+			.header("Content-type", "application/json")
+			.header("Authorization", "Bearer " + invalidToken)
+			.body(newMovie)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post("/movies")
+		.then()
+			.statusCode(401);
 	}
 }
